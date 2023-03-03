a3f13878f0a5fc7abe76704999a5b0eae7d55690
vishymails <vishymails@gmail.com>
2023-03-03 12:40:24 +0530

    added

---
+++ properties-access-service/src/main/java/com/bvr/PropertiesAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertiesAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertiesAccessServiceApplication.class, args);
+	}
+
+}
